d805427744a323d9c2ee6e6af48eccbf57a37ab7
Quang Đạt Trần <tranthithanhhuong@MacBook-Air-cua-Quang-at.local>
2023-04-25 22:56:28 +0700

    big update

---
+++ bai 2.js
@@ -0,0 +1,14 @@
+function isPrimeNumber(n) {
+    if (n <= 1) {
+      return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  console.log(isPrimeNumber(11));
